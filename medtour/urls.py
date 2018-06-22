ce1b7658f0a6c7790b4e247362e1472282770af2
Mohit Kumar Yadav <mohitkyadav@outlook.com>
2018-06-22 07:25:58 +0530

    refractor project

---
+++ medtour/urls.py
@@ -0,0 +1,9 @@
+from django.conf.urls import include, url
+from django.urls import path
+
+from django.contrib import admin
+admin.autodiscover()
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+]
