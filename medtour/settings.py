b19efb47c602c7f537283eac01ff1f06c3ba02cc
Mohit Kumar Yadav <mohitkyadav@outlook.com>
2018-06-22 08:09:56 +0530

    setup uikit in  base

--- medtour/settings.py
+++ medtour/settings.py
@@ -125,5 +125,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.0/howto/static-files/
 
 STATIC_URL = '/static/'
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, "medtour/static"),
+]
 
 django_heroku.settings(locals())
