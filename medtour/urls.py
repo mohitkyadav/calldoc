aef74ca866e394d66696a73ce092343514a8ccf3
Mohit Kumar Yadav <mohitkyadav@outlook.com>
2018-06-22 07:37:12 +0530

    landing view

--- medtour/urls.py
+++ medtour/urls.py
@@ -2,8 +2,10 @@ from django.conf.urls import include, url
 from django.urls import path
 
 from django.contrib import admin
+import landing.views
 admin.autodiscover()
 
 urlpatterns = [
+    url(r'^$', landing.views.land, name='land'),
     path('admin/', admin.site.urls),
 ]
