c9158dcdcd8fa46fc3a2b91fcb2ec658ebc6d230
Mohit Kumar Yadav <mohitkyadav@outlook.com>
2018-07-13 12:40:42 +0530

    customize hospital model

--- hospital/models.py
+++ hospital/models.py
@@ -1,8 +1,6 @@
 from django.contrib.auth.models import User
 from django.db import models
 
-from landing.models import Country, Region, City
-
 
 class Hospital(models.Model):
     class Meta:
@@ -11,18 +9,8 @@ class Hospital(models.Model):
         verbose_name_plural = 'Hospitals'
 
     user = models.OneToOneField(User, on_delete=models.CASCADE)
-    name = models.CharField(max_length=5000, null=True, blank=True)
+    name = models.CharField(max_length=1000, null=True, blank=True)
     address = models.TextField(max_length=5000, null=True, blank=True)
-    dob = models.DateField(blank=True, null=True, help_text='Date of signing up')
-
-    mail_notifications_allowed = models.BooleanField(default=True,
-                                                     help_text='Get all your notifications via mail')
-    newsletter = models.BooleanField(default=True, help_text='Get notifications our new services and features')
-    avatar = models.URLField(null=True, blank=True, help_text='Profile picture URL')
-    avatar_small = models.URLField(null=True, blank=True, help_text='Profile picture smaller URL')
-    country = models.ForeignKey(Country, null=True, blank=True, on_delete=models.PROTECT)
-    state = models.ForeignKey(Region, null=True, blank=True, on_delete=models.PROTECT)
-    city = models.ForeignKey(City, null=True, blank=True, on_delete=models.PROTECT)
 
     def __str__(self):
         return self.name
