1bca109ac57a0945e4fd83cb7f2fd01f56ccc3d3
Mohit Kumar Yadav <mohitkyadav@outlook.com>
2018-07-18 19:53:53 +0530

    handle post search request

--- hospital/views.py
+++ hospital/views.py
@@ -2,6 +2,7 @@ from django.contrib import messages
 from django.http import JsonResponse
 from django.shortcuts import render, redirect, get_object_or_404
 from django.views import View
+
 from .models import Hospital
 
 
@@ -18,4 +19,12 @@ class HospitalsAll(View):
         hospitals = Hospital.objects.all()
         return render(request, 'hospital/hospital-list.html', {
             'hospitals': hospitals,
+        })
+
+    def post(self, request):
+        search_box_city_value = request.POST.get('search_box_city')
+        hospitals = Hospital.objects.all()
+        return render(request, 'hospital/hospital-list.html', {
+            'hospitals': hospitals,
+            'search_box_city_value': search_box_city_value,
         })
