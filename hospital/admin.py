fb469c9b00235174c8cebb2951e035fab7b91d34
Aashutosh Rathi <aashutoshrathi@gmail.com>
2018-07-25 13:32:04 +0530

    Added to admin and cleanup

--- hospital/admin.py
+++ hospital/admin.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
 
-from .models import Hospital, Specialisation, Doctor
+from .models import Hospital, Specialisation, Doctor, Appointment
 
 admin.site.register(Hospital)
 admin.site.register(Specialisation)
 admin.site.register(Doctor)
+admin.site.register(Appointment)
