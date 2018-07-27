329775f3c5146a6e71806f9858a805790f3c664c
Aashutosh Rathi <aashutoshrathi@gmail.com>
2018-07-27 22:31:41 +0530

    Appointment done

--- hospital/forms.py
+++ hospital/forms.py
@@ -1,6 +1,5 @@
 import datetime
 from datetimewidget.widgets import DateTimeWidget
-from django.forms import ModelForm
 from .models import Hospital, Appointment
 from django import forms
 from django.contrib.auth.models import User
@@ -52,20 +51,13 @@ class HospitalForm(forms.ModelForm):
         }
 
 
-class AppointmentForm(ModelForm):
-    def __init__(self, **kwargs):
-        super().__init__()
-        self.fields['doctor'].label = 'Doctor'
-        if kwargs:
-            self.fields['doctor'].initial = kwargs.pop('doctor')
-            self.fields['doctor'].queryset = kwargs.pop('doctors')
+class AppointmentForm(forms.ModelForm):
+    def __init__(self, *args, **kwargs):
+        super(AppointmentForm, self).__init__(*args, **kwargs)
         self.fields['start_date'].label = 'Start time'
         self.fields['end_date'].label = 'End time'
         self.fields['patients_remarks'].label = 'Remarks'
 
-        self.fields['doctor'].widget.attrs.update({
-            'class': 'uk-width-auto uk-select'
-        })
         self.fields['start_date'].widget.attrs.update({
             'class': 'uk-width-auto uk-input'
         })
@@ -76,37 +68,11 @@ class AppointmentForm(ModelForm):
             'class': 'uk-width-auto uk-input'
         })
 
-    def clean(self):
-        cleaned_data = super(AppointmentForm, self).clean()
-        doctor = cleaned_data.get('doctor')
-        start_time = cleaned_data.get('start_date')
-        end_time = cleaned_data.get('end_date')
-        if not doctor:
-            raise forms.ValidationError("Doctor is a required field.")
-        if start_time and end_time:
-            if start_time > end_time:
-                msg = u"Start time must be before the end time"
-                self._errors['start_time'] = self.error_class([msg])
-        elif not start_time:
-            msg = u"Appointment start time is a required field"
-            self._errors['start_time'] = self.error_class([msg])
-        elif not end_time:
-            msg = u"Appointment time is a required field"
-            self._errors['end_time'] = self.error_class([msg])
-        return cleaned_data
-
-    def save(self, **kwargs):
-        instance = super(AppointmentForm, self).save(commit=False)
-        instance.save()
-        return instance
-
     class Meta:
         model = Appointment
-        fields = {'doctor', 'patients_remarks', 'start_date', 'end_date'}
+        fields = {'patients_remarks', 'start_date', 'end_date'}
         dateTimeOptions = {
-            'format': 'dd/mm/yyyy HH:ii P',
             'autoclose': True,
-            'showMeridian': True,
             'startDate': str(datetime.datetime.today().date()),
             'endDate': str(datetime.datetime.today().date() + datetime.timedelta(7)),
         }
